81d4d808b3ac8f67f4f92a856d29d5cc42cf112a
christopherzulu <christopherzulu53@gmail.com>
2022-06-23 06:27:26 +0200

    WeDevelopementBlog

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,12 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
-import Contact from "./Contact"
+
 
 function Footer(){
     return(
       
         <>
        
-        <Contact/>
+    
        
        
        <footer className="footer-area footer-bg2 " 
